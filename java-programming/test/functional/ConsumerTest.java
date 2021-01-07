ae06588e9c43323536eed250358e29d46de9efd0
lowelllll <yejin2651@naver.com>
2021-01-08 00:49:05 +0900

    [functional interface] consumer-2

--- java-programming/test/functional/ConsumerTest.java
+++ java-programming/test/functional/ConsumerTest.java
@@ -1,14 +1,10 @@
 package functional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
-
-import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ConsumerTest {
 
     @Test
-    public void test() {
+    public void test1() {
         List<Integer> numbers = new ArrayList();
 
         Consumer<List<Integer>> addConsumer = (list) -> list.add(0, 1);
@@ -29,5 +25,25 @@ public class ConsumerTest {
         addConsumer.andThen(clearConsumer).accept(numbers);
         // consumer interface는 결과를 리턴하지 않음. andThen은 functional interface의 호출 순서만 정함
         assertTrue(numbers.isEmpty());
+    }
+
+    @Test
+    public void test2() {
+        andThen();
+        accept();
+    }
+
+    private void accept() {
+        // lambda식은 functional interface의 인스턴스를 생성함.
+        final Consumer<String> greetings = value -> System.out.println("hello" + value);
+        greetings.accept("lowell");
+    }
+
+    private void andThen() {
+        final Consumer<String> consumer1 = value -> System.out.println("consumer 1 " + value);
+        final Consumer<String> consumer2 = value -> System.out.println("consumer 2 " + value);
+
+        consumer1.andThen(consumer2).accept("is Consume");
+
     }
 }
